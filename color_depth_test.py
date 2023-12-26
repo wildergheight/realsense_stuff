782e97301824475cfe9ebde824a887d57d5829d9
dylan <dylan.klein2018@gmail.com>
2023-12-25 23:34:59 -0800

    uh

--- color_depth_test.py
+++ color_depth_test.py
@@ -14,11 +14,14 @@ import cv2
 
 x_coord = 50
 y_coord = 50
+button_pressed = False
+check_hand = False
 
 
 def click_event(event, x, y, flags, params):
     global x_coord
     global y_coord
+    global button_pressed
     # checking for left mouse clicks
     if event == cv2.EVENT_LBUTTONDOWN:
         # displaying the coordinates
@@ -26,6 +29,7 @@ def click_event(event, x, y, flags, params):
         # print(x, ' ', y)
         x_coord = y
         y_coord = x
+        button_pressed = True
 
 
 # Create a pipeline
@@ -54,7 +58,6 @@ if not found_rgb:
 
 # config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-
 
 if device_product_line == 'L500':
     config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
@@ -103,8 +106,17 @@ try:
         color_image = np.asanyarray(color_frame.get_data())
         # rgb_value_center = color_image[y_coord, x_coord]
         rgb_value_center = color_image[x_coord, y_coord]
+        depth_at_center = depth_image[x_coord, y_coord]
 
-        print(rgb_value_center)
+        HFOV = 69
+        VFOV = 42
+        center_of_image = [640, 360]
+
+        h_angle = ((y_coord-center_of_image[0])/center_of_image[0])*(HFOV/2)
+        v_angle = ((x_coord - center_of_image[1]) / center_of_image[1]) * (VFOV / 2)
+        # print(h_angle)
+        print(v_angle)
+        # print(rgb_value_center)
 
         # Create a circular mask
         mask = np.zeros_like(color_image)
@@ -116,20 +128,21 @@ try:
         if start_point_y < 0:
             start_point_y = 0
         end_point_x = x_coord + 5
-        if end_point_x > 480:
-            end_point_x = 480
+        if end_point_x > 720:
+            end_point_x = 720
         end_point_y = y_coord + 5
-        if end_point_y > 640:
-            end_point_y = 640
+        if end_point_y > 1280:
+            end_point_y = 1280
         cv2.rectangle(mask, (start_point_y, start_point_x), (end_point_y, end_point_x), (255, 255, 255),
                       thickness=cv2.FILLED)
         # Apply the mask to the image data to extract the circular region
         result = cv2.bitwise_and(color_image, mask)
 
         # If you want to get the average color in the circle
-        new_arr = result[start_point_x:end_point_x+1,start_point_y:end_point_y+1]
+        new_arr = result[start_point_x:end_point_x + 1, start_point_y:end_point_y + 1]
         average_color = np.mean(new_arr, axis=(0, 1))
-        print(average_color)
+        average_color = np.around(average_color, decimals=2)
+        # print(average_color)
 
         images = color_image
 
@@ -150,6 +163,22 @@ try:
         # Using cv2.circle() method
         # Draw a circle with blue line borders of thickness of 2 px
         image = cv2.circle(images, center_coordinates, radius, color, thickness)
+
+        # Hand Detection
+        if button_pressed:
+            stored_r = average_color[2]
+            stored_g = average_color[1]
+            stored_b = average_color[0]
+            button_pressed = False
+            check_hand = True
+
+        if check_hand:
+            diff_r = abs(stored_r - average_color[2])
+            diff_g = abs(stored_g - average_color[1])
+            diff_b = abs(stored_b - average_color[0])
+
+            # if diff_r > 20 or diff_b > 20 or diff_g > 20:
+                # print("HAND DETECTED")
 
         cv2.imshow('Align Example', images)
 
